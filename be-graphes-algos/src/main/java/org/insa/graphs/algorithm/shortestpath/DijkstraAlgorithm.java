9d2697f001509654e4b2664d026727c6bc18c26f
Artus13 <artusdeumier@gmail.com>
2023-04-12 10:26:40 +0200

    Label Fini et debut Dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,16 +1,45 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.ArrayList;
+
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+
+import org.insa.graphs.algorithm.shortestpath.Label;
+
+import org.insa.graphs.model.Node ;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
         super(data);
     }
 
+    private ArrayList <Label> listeLabels = new ArrayList <Label>();
+
     @Override
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
         // TODO:
+
+        for (int i = 0; i < data.getGraph().getNodes().size(); i++) {
+            listeLabels.add(null);
+        }
+        
+        //Initialisation
+        for (int i = 0; i < data.getGraph().getNodes().size(); i++) {
+            listeLabels.get(i).setSommetCourant(data.getGraph().getNodes().get(i)) ;
+            listeLabels.get(i).setMarque(false) ;
+            listeLabels.get(i).setCoutRealise(Double.POSITIVE_INFINITY) ;
+            listeLabels.get(i).setPere(null);
+        }
+
+        listeLabels.get(0).setCoutRealise(0.0) ;
+        BinaryHeap<Label> tas = new BinaryHeap<Label>() ;
+        tas.BinaryHeap() ; 
+        insert(listeLabels.get(0))
+
+
         return solution;
     }
 
---
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -0,0 +1,46 @@
+package org.insa.graphs.algorithm.shortestpath;
+
+import org.insa.graphs.model.Node;
+
+public class Label {
+   private Node sommetCourant ;
+   private Boolean marque ;
+   private Double coutRealise ;
+   private Node pere ;
+
+   public Node getSommetCourant () {
+      return this.sommetCourant ;
+   }
+
+   public void setSommetCourant (Node sommetCourant) {
+      this.sommetCourant = sommetCourant ;
+   }
+
+   public void setCoutRealise (Double coutRealise) {
+      this.coutRealise = coutRealise ;
+   }
+
+   public Boolean getMarque () {
+      return this.marque ;
+   }
+
+   public void setMarque (Boolean marque) {
+      this.marque = marque ;
+   }
+
+   public Double getCoutRealise () {
+      return this.coutRealise ;
+   }
+
+   public Node getPere () {
+      return this.pere ;
+   }
+
+   public void setPere (Node pere) {
+      this.pere = pere ;
+   }
+
+   public Double getCost () {
+      return this.coutRealise ;
+   }
+}
